a5c24e9ab4e642d87a726478150a3509a2324dde
Vladimir <vladkodoktorov@gmail.com>
2021-12-02 09:47:28 +0100

    annotations and functions

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,4 +1,4 @@
-let apples: number = 5;
+let apples = 5;
 let speed: string = 'ddddd';
 let hasName: boolean = true;
 
@@ -7,3 +7,35 @@ let nothing: undefined = undefined;
 
 //built in objects
 let now: Date = new Date();
+
+
+//function
+
+const logNumber: (i: number) => void = (i: number) => {
+    console.log(i);
+};
+
+const json = '{"x": 10, "y": 20}';
+const coordinates: {x:number; y:number} = JSON.parse(json);
+console.log(coordinates);
+
+//when we declare a variable on one line and initialize it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let index = 0; index < words.length; index++) {
+    if (words[index] === 'green') {
+        foundWord = true;
+    }
+}
+
+//variables whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let index = 0; index < numbers.length; index++) {
+    if (numbers[index] > 0) {
+        numberAboveZero = numbers[index];
+    }
+}
